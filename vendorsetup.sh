d6478162a6937278ea0128982cb8d54e3b3e1313
HinohArata <mmgcntk@gmail.com>
2024-04-24 23:00:20 +0700

    surya: vendorsetup: fix undefined link

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/HinohArata/surya_device.git -b hardware hardware/xiaomi
 
 # Vendor
-git clone https://github.com/HinohArata/surya_device.git vendor/xiaomi/surya
+git clone https://github.com/HinohArata/surya_vendor.git vendor/xiaomi/surya
 
 # Kernel
-git clone --depth=1 https://github.com/HinohArata/surya.git -b skyhigh-new kernel/xiaomi/surya
+git clone --depth=1 https://github.com/HinohArata/sky_kernel.git -b skyhigh-new kernel/xiaomi/surya
