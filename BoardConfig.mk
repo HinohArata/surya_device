ca89a43a899ebcdeaa0089758fa1d3031a7f54d8
HinohArata <mmgcntk@gmail.com>
2024-04-24 19:59:29 +0700

    surya: Set HWUI compile for Perfomance

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,6 +85,9 @@ ODM_MANIFEST_SKUS += surya
 ODM_MANIFEST_SURYA_FILES := \
     $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml \
     $(DEVICE_PATH)/configs/hidl/manifest-qva.xml
+    
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_surya
