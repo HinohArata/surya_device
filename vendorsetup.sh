5fd5e80ef7337d431632bbffbee60ce31b46047a
HinohArata <mmgcntk@gmail.com>
2024-04-24 22:45:20 +0700

    surya: vendorsetup: clone vendor, kernel, and hardware directly

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Hardware
+git clone https://github.com/HinohArata/surya_device.git -b hardware hardware/xiaomi
+
+# Vendor
+git clone https://github.com/HinohArata/surya_device.git vendor/xiaomi/surya
+
+# Kernel
+git clone --depth=1 https://github.com/HinohArata/surya.git -b skyhigh-new kernel/xiaomi/surya
